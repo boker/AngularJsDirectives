f5ad36eea7ef57eaf054dd6429a6c70b9b6d971a
naresh <narunaram@hotmail.com>
2014-07-17 07:42:24 +0530

    wip - link

--- LinkExample/app.js
+++ LinkExample/app.js
@@ -1,29 +1,12 @@
 'use strict';
 
 angular.module('app', [])
-.controller('Ctrl', function($scope){
-	$scope.ctrlDate = '07/17/2014';
-})
-.directive('datePicker', function(){
+.directive('myToggle', function(){
 	return {
-		restrict: 'E',
-		require: '?ngModel',
-		template: 'Date Picker: <input type="text" ng-click="selectDate()" ng-model="selectedDate"></input>',
+		restrict: 'A',
 		link: function(scope, elm, attrs, ngModel){
-			ngModel = ngModel || {$setViewValue : angular.noop};
+			
 
-			scope.selectDate = function(){
-				elm.datepicker('dialog', null, function(value){
-					scope.$apply(function(){
-						scope.selectedDate = value;
-						ngModel.$setViewValue(value);
-					});
-				});
-			};
-
-			ngModel.$render = function(){
-				scope.selectedDate = ngModel.$modelValue;
-			};
 		},
 		scope: true
 	}
